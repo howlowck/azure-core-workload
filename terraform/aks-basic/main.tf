cfda5e6fc32740fa4474b754fbd9e9567dca3e7a
Hao Luo <howlowck@gmail.com>
2022-10-26 21:49:09 -0500

    removed outputs
    Signed-off-by: Hao Luo <howlowck@gmail.com>

--- terraform/aks-basic/main.tf
+++ terraform/aks-basic/main.tf
@@ -37,11 +37,11 @@ resource "azurerm_kubernetes_cluster" "core" {
   }
 }
 
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.core.kube_config.0.client_certificate
-}
+# output "client_certificate" {
+#   value = azurerm_kubernetes_cluster.core.kube_config.0.client_certificate
+# }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.core.kube_config_raw
-  sensitive = true
-}
+# output "kube_config" {
+#   value = azurerm_kubernetes_cluster.core.kube_config_raw
+#   sensitive = true
+# }
